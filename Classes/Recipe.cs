f27c3dd2c5c38f046c07649dafa67e94c92778e4
Koki B <st10139225@rcconnect.edu.za>
2023-04-22 15:36:04 +0000

    completed step class

---
+++ Classes/Recipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10139225_K_Baholo_Part1.Classes
+{
+    internal class Recipe
+    {
+        //To store all the steps
+        int Number_of_steps = 0;
+        List<Steps> List_of_Steps = new List<Steps>();
+
+
+        private void addSteps()
+        {
+            string Instruction;
+            for (int count = 0; count <= Number_of_steps; count++)
+            {
+                Console.WriteLine("Please enter step " + count + ": ");
+                Instruction = Console.ReadLine();
+                Steps instruction = new Steps(count, Instruction);
+                List_of_Steps.Add(instruction);
+            }
+        }
+
+    }
+}
---
+++ Classes/Steps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10139225_K_Baholo_Part1.Classes
+{
+    internal class Steps
+    {
+
+        private int step_number;
+        private string Description;
+
+        public Steps(int Step_number, string description)
+            {
+
+                this.Description = description;
+                this.step_number = Step_number;
+            }
+
+    public string getStep()
+        {
+            return "" + step_number + ".) " + Description + ".";
+        }
+
+
+     
+
+    }
+}
